268747a492a5624b083eb3e5004140887a30a976
algorithm89 <alexei.go89@gmail.com>
2026-04-18 11:37:34 -0400

    Pipeline uses checkout, no repo needed on server

--- test_connections.py
+++ test_connections.py
@@ -25,7 +25,7 @@ try:
     import mysql.connector
     conn = mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
-        port=int(os.getenv("DB_PORT", "3306")),
+        port=int(os.getenv("DB_PORT") or "3306"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
